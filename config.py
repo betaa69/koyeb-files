fb5de3357c1b870b29574a80d971a32e13ae61fb
betaa69 <114795795+betaa69@users.noreply.github.com>
2024-06-17 12:55:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "<b>❌Don't send me messages directly I Only work for @MoviezAddaKA >_< \n\nUse /join command to get active channels list.</b>"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
